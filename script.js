552d881beb11b19b80f31a6762bbd3d3527b6ee1
Alesh Patil <aleshpatil24032002@gmail.com>
2024-02-12 09:52:36 +0530

    JS functionality added

---
+++ script.js
@@ -0,0 +1,101 @@
+let currentValue='';
+document.querySelector('#display').value =currentValue;
+
+
+function fc()
+{
+    currentValue = '';
+    document.querySelector('#display').value =currentValue;
+}
+function f1()
+{
+    currentValue = currentValue + '1';
+    document.querySelector('#display').value =currentValue;
+}
+function f2()
+{
+    currentValue = currentValue + '2';
+    document.querySelector('#display').value =currentValue;
+}
+function f3()
+{
+    currentValue = currentValue + '3';
+    document.querySelector('#display').value =currentValue;
+}
+function f4()
+{
+    currentValue = currentValue + '4';
+    document.querySelector('#display').value =currentValue;
+}
+function f5()
+{
+    currentValue = currentValue + '5';
+    document.querySelector('#display').value =currentValue;
+}
+function f6()
+{
+    currentValue = currentValue + '6';
+    document.querySelector('#display').value =currentValue;
+}
+function f2()
+{
+    currentValue = currentValue + '2';
+    document.querySelector('#display').value =currentValue;
+}
+function f7()
+{
+    currentValue = currentValue + '7';
+    document.querySelector('#display').value =currentValue;
+}
+function f8()
+{
+    currentValue = currentValue + '8';
+    document.querySelector('#display').value =currentValue;
+}
+function f9()
+{
+    currentValue = currentValue + '9';
+    document.querySelector('#display').value =currentValue;
+}function f0()
+{
+    currentValue = currentValue + '0';
+    document.querySelector('#display').value =currentValue;
+}
+function add()
+{
+    currentValue = currentValue + '+';
+    document.querySelector('#display').value =currentValue;
+}
+function sub()
+{
+    currentValue = currentValue + '-';
+    document.querySelector('#display').value =currentValue;
+}
+function mul()
+{
+    currentValue = currentValue + '*';
+    document.querySelector('#display').value =currentValue;
+}
+function div()
+{
+    currentValue = currentValue + '/';
+    document.querySelector('#display').value =currentValue;
+}
+function dot()
+{
+    currentValue = currentValue + '.';
+    document.querySelector('#display').value =currentValue;
+}
+function ans()
+{
+    currentValue = eval(currentValue);
+    document.querySelector('#display').value =currentValue;
+}
+function ac()
+{
+    currentValue = currentValue;
+    currentValue--;
+    document.querySelector('#display').value =currentValue;
+}
+
+
